c137ca0518bd78cfbad46092ce22803074117fe8
Aske Simon Christensen <askesc@google.com>
2020-05-04 17:29:23 +0200

    Fix utf8_test to match breaking change to UTF-8 encoding and decoding

--- test/utf8_test.dart
+++ test/utf8_test.dart
@@ -56,15 +56,14 @@ main() {
     final Pointer<Utf8> converted = Utf8.toUtf8(start).cast();
     final int length = Utf8.strlen(converted);
     final Uint8List end = converted.cast<Uint8>().asTypedList(length + 1);
-    expect(end, equals([237, 160, 128, 225, 128, 128, 0]));
+    expect(end, equals([239, 191, 189, 225, 128, 128, 0]));
     free(converted);
   });
 
   test("fromUtf8 unpaired surrogate", () {
     final Pointer<Utf8> utf8 =
         _bytesFromList([237, 160, 128, 225, 128, 128, 0]).cast();
-    final String end = Utf8.fromUtf8(utf8);
-    expect(end, equals(String.fromCharCodes([0xD800, 0x1000])));
+    expect(() => Utf8.fromUtf8(utf8), throwsA(isFormatException));
   });
 
   test("fromUtf8 invalid", () {
