98e8755702c8c9a877a7c36e2e56d9c8f08439b8
J-P Nurmi <jpnurmi@gmail.com>
2020-11-18 09:21:20 +0100

    Tweak the docs (#63)
    Be consistent about zero- vs. null-terminated (the latter was more
    commonly used, but using the former was suggested in #61). Furthermore,
    align UTF-8/16 spelling and fix a couple of minor typos and formatting
    issues.

--- lib/src/utf16.dart
+++ lib/src/utf16.dart
@@ -8,21 +8,21 @@ import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
 
-/// [Utf16] implements conversion between Dart strings and null-terminated
+/// [Utf16] implements conversion between Dart strings and zero-terminated
 /// UTF-16 encoded "char*" strings in C.
 ///
-/// [Utf16] is respresented as a struct so that `Pointer<Utf16>` can be used in
+/// [Utf16] is represented as a struct so that `Pointer<Utf16>` can be used in
 /// native function signatures.
 class Utf16 extends Struct {
-  /// Convert a [String] to a Utf16-encoded null-terminated C string.
+  /// Convert a [String] to a UTF-16 encoded zero-terminated C string.
   ///
-  /// If 'string' contains NULL bytes, the converted string will be truncated
+  /// If [string] contains NULL characters, the converted string will be truncated
   /// prematurely. Unpaired surrogate code points in [string] will be preserved
-  /// in the UTF-8 encoded result. See [Utf8Encoder] for details on encoding.
+  /// in the UTF-16 encoded result. See [Utf16Encoder] for details on encoding.
   ///
   /// Returns a malloc-allocated pointer to the result.
-  static Pointer<Utf16> toUtf16(String s) {
-    final units = s.codeUnits;
+  static Pointer<Utf16> toUtf16(String string) {
+    final units = string.codeUnits;
     final Pointer<Uint16> result = allocate<Uint16>(count: units.length + 1);
     final Uint16List nativeString = result.asTypedList(units.length + 1);
     nativeString.setAll(0, units);
