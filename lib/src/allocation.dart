01a67e972e506ffd877737bfb61072c0f0c5e23d
Daco Harkes <dacoharkes@google.com>
2021-01-09 10:49:02 +0100

    Add CallocAllocator

--- lib/src/allocation.dart
+++ lib/src/allocation.dart
@@ -15,6 +15,11 @@ typedef PosixMalloc = Pointer Function(int);
 final PosixMalloc posixMalloc =
     stdlib.lookupFunction<PosixMallocNative, PosixMalloc>('malloc');
 
+typedef PosixCallocNative = Pointer Function(IntPtr num, IntPtr size);
+typedef PosixCalloc = Pointer Function(int num, int size);
+final PosixCalloc posixCalloc =
+    stdlib.lookupFunction<PosixCallocNative, PosixCalloc>('calloc');
+
 typedef PosixFreeNative = Void Function(Pointer);
 typedef PosixFree = void Function(Pointer);
 final PosixFree posixFree =
@@ -35,6 +40,8 @@ typedef WinHeapFreeNative = Int32 Function(
 typedef WinHeapFree = int Function(Pointer heap, int flags, Pointer memory);
 final WinHeapFree winHeapFree =
     stdlib.lookupFunction<WinHeapFreeNative, WinHeapFree>('HeapFree');
+
+const int HEAP_ZERO_MEMORY = 8;
 
 /// Allocates memory on the native heap.
 ///
@@ -64,12 +71,15 @@ void free(Pointer pointer) => malloc.free(pointer);
 
 /// Manages memory on the native heap.
 ///
+/// Does not intialize newly allocated memory to zero. Use [CallocAllocator]
+/// for zero-intialized memory on allocation.
+///
 /// For POSIX-based systems, this uses malloc and free. On Windows, it uses
 /// HeapAlloc and HeapFree against the default public heap.
 class MallocAllocator implements Allocator {
   const MallocAllocator();
 
-  /// Allocates memory on the native heap.
+  /// Allocates uninitialized memory on the native heap.
   ///
   /// For POSIX-based systems, this uses malloc. On Windows, it uses HeapAlloc
   /// against the default public heap.
@@ -113,3 +123,59 @@ class MallocAllocator implements Allocator {
 }
 
 const malloc = MallocAllocator();
+
+/// Manages memory on the native heap.
+///
+/// Initializes newly allocated memory to zero.
+///
+/// For POSIX-based systems, this uses calloc and free. On Windows, it uses
+/// HeapAlloc with [HEAP_ZERO_MEMORY] and HeapFree against the default public
+/// heap.
+class CallocAllocator implements Allocator {
+  const CallocAllocator();
+
+  /// Allocates zero-intialized memory on the native heap.
+  ///
+  /// For POSIX-based systems, this uses malloc. On Windows, it uses HeapAlloc
+  /// against the default public heap.
+  ///
+  /// Throws an ArgumentError on failure to allocate.
+  // TODO: Stop ignoring alignment if it's large, for example for SSE data.
+  @override
+  Pointer<T> allocate<T extends NativeType>(int numBytes, {int? alignment}) {
+    Pointer<T> result;
+    if (Platform.isWindows) {
+      result = winHeapAlloc(processHeap, /*flags=*/ HEAP_ZERO_MEMORY, numBytes)
+          .cast();
+    } else {
+      result = posixCalloc(numBytes, 1).cast();
+    }
+    if (result.address == 0) {
+      throw ArgumentError('Could not allocate $numBytes bytes.');
+    }
+    return result;
+  }
+
+  /// Releases memory on the native heap.
+  ///
+  /// For POSIX-based systems, this uses free. On Windows, it uses HeapFree
+  /// against the default public heap. It may only be used against pointers
+  /// allocated in a manner equivalent to [allocate].
+  ///
+  /// Throws an ArgumentError on failure to free.
+  ///
+  // TODO(dartbug.com/36855): Once we have a ffi.Bool type we can use it instead
+  // of testing the return integer to be non-zero.
+  @override
+  void free(Pointer pointer) {
+    if (Platform.isWindows) {
+      if (winHeapFree(processHeap, /*flags=*/ 0, pointer) == 0) {
+        throw ArgumentError('Could not free $pointer.');
+      }
+    } else {
+      posixFree(pointer);
+    }
+  }
+}
+
+const calloc = CallocAllocator();
