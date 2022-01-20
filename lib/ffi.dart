72aafc25c7a045f9f267c606536ee92fd6d43392
Daco Harkes <dacoharkes@google.com>
2022-01-20 15:55:34 +0100

    update with c_type.dart from 2.17 SDK
    https://dart-review.googlesource.com/c/sdk/+/228541

--- lib/ffi.dart
+++ lib/ffi.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/abi_specific_ints.dart';
 export 'src/allocation.dart' show calloc, malloc;
 export 'src/arena.dart';
+export 'src/c_type.dart';
 export 'src/utf8.dart';
 export 'src/utf16.dart';
