2791197c2e9483a734945efa6cc957dbe8f369b1
Tim Sneath <timsneath@google.com>
2022-06-19 09:20:41 -0700

    Add tests that mirror dart/sdk tests

--- test/allocation_test.dart
+++ test/allocation_test.dart
@@ -21,4 +21,21 @@ void main() async {
       calloc.free(mem);
     }
   });
+
+  test('testPointerAllocateTooLarge', () {
+    // Try to allocate something that doesn't fit in 64 bit address space.
+    int maxInt = 9223372036854775807; // 2^63 - 1
+    expect(() => calloc<Uint8>(maxInt), throwsA(isA<ArgumentError>()));
+
+    // Try to allocate almost the full 64 bit address space.
+    int maxInt1_8 = 1152921504606846975; // 2^60 -1
+    expect(() => calloc<Uint8>(maxInt1_8), throwsA(isA<ArgumentError>()));
+  });
+
+  test('testPointerAllocateNegative', () {
+    // Passing in -1 will be converted into an unsigned integer. So, it will try
+    // to allocate SIZE_MAX - 1 + 1 bytes. This will fail as it is the max
+    // amount of addressable memory on the system.
+    expect(() => calloc<Uint8>(-1), throwsA(isA<ArgumentError>()));
+  });
 }
